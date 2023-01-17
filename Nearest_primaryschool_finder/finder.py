575943deeb09fda32c87b353e8097a7339092c7a
mwolska <monika.wolska1404@gmail.com>
2023-01-17 15:49:18 +0100

    Work on the functions to change data frame, wrap data and use harvestine formula to find minimum distance

--- Nearest_primaryschool_finder/finder.py
+++ Nearest_primaryschool_finder/finder.py
@@ -1,27 +1,52 @@
 import math
 import pandas as pd
-
+from requests import get
+from bs4 import BeautifulSoup
 
 file = 'warsaw_primary_schools.xlsx'
 
 
-def finder(lat1, lon1, lat2, lon2):
-# using harvestine formula
-        dLat = (lat2 - lat1) * math.pi / 180.0
-        dLon = (lon2 - lon1) * math.pi / 180.0
 
-# converting to radians
-        lat1 = (lat1) * math.pi / 180.0
-        lat2 = (lat2) * math.pi / 180.0
 
-# applying formula
-        a = (pow(math.sin(dLat / 2), 2) +
-             pow(math.sin(dLon / 2), 2) *
-             math.cos(lat1) * math.cos(lat2));
-        rad = 6371
-        c = 2 * math.asin(math.sqrt(a))
+def add_coordinates(df):
+    url_beginning = r"https://www.google.pl/maps/place/"
+    df['Latitude'] = '0'
+    df['Longitude'] = '0'
 
-        return rad * c
+    for index, row in df.itterrows():
+        URL = url_beginning + row[16] + r"+" + str(row[17]) + r",+" + row[19] + "+Warszawa"
+        page = get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        for object in soup.find_all('div', class_='mLuXec'):
+            price = object.find('div', class_='mLuXec').get_text().strip().split(", ")
+            df[index, 'Latitude'] = price[0]
+            df[index, 'Latitude'] = price[1]
+
+    return df
+
+
+def finder(df):
+
+
+    lat1, lon1, lat2, lon2
+    # using harvestine formula
+    dLat = (lat2 - lat1) * math.pi / 180.0
+    dLon = (lon2 - lon1) * math.pi / 180.0
+
+    # converting to radians
+    lat1 = (lat1) * math.pi / 180.0
+    lat2 = (lat2) * math.pi / 180.0
+
+    # applying formula
+    a = (pow(math.sin(dLat / 2), 2) +
+         pow(math.sin(dLon / 2), 2) *
+         math.cos(lat1) * math.cos(lat2))
+
+    rad = 6371
+    c = 2 * math.asin(math.sqrt(a))
+
+    return rad * c
 
 
 def find_school(logopedist, psychologist, pedagogue, language, students, school_type):
@@ -30,10 +55,8 @@ def find_school(logopedist, psychologist, pedagogue, language, students, school_
                      data['Kategoria uczniów'].eq(students), data["Języki nauczane"].str.constains(language), data["Czy zatrudnia logopedę"].isin(logopedist),
                      data["Czy zatrudnia pedagoga"].isin(pedagogue), data["Czy zatrudnia psychologa"].isin(psychologist)]]
 
-    school = "Pass"
-    return school
+    schools = add_coordinates(df)
 
-
-
+    return finder(schools)
 
 
