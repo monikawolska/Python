c3bd982a58e6fb50d8f65f758f01cb6ba8e2629f
mwolska <monika.wolska1404@gmail.com>
2023-06-22 09:52:13 +0200

    work on finder method

--- Nearest_primaryschool_finder/finder.py
+++ Nearest_primaryschool_finder/finder.py
@@ -8,8 +8,8 @@ file = 'warsaw_primary_schools.xlsx'
 
 def add_coordinates(df):
     url_beginning = r"https://www.google.pl/maps/place/"
-    df['Latitude'] = '0'
-    df['Longitude'] = '0'
+    df['Latitude'] = 0
+    df['Longitude'] = 0
 
     for index, row in df.itterrows():
         URL = url_beginning + row[16] + r"+" + str(row[17]) + r",+" + row[19] + "+Warszawa"
@@ -24,7 +24,7 @@ def add_coordinates(df):
     return df
 
 
-def finder(df: object, city: object, street: object, number: object, postalcode: object) -> object:
+def finder(df, city, street, number, postalcode):
     url_beginning = r"https://www.google.pl/maps/place/"
     URL = url_beginning + street + r"+" + str(number) + r",+" + postalcode + "+" + city
     page = get(URL)
@@ -32,21 +32,19 @@ def finder(df: object, city: object, street: object, number: object, postalcode:
 
     for object in soup.find_all('div', class_='mLuXec'):
         price = object.find('div', class_='mLuXec').get_text().strip().split(", ")
-        lat_user = price[0]
-        lon_user = price[1]
+        lat_user = float(price[0])
+        lon_user = float(price[1])
 
-    df['distance'] = [sqrt(pow((price[0] - df['Latitude']), 2) + pow((price[1] - df['Longitude']), 2))]
+    df['distance'] = [math.sqrt(pow((lat_user - df['Latitude']), 2) + pow((lon_user - df['Longitude']), 2))]
 
     minim = min(df['distance'])
-    return df[df['distance'] == minim]
+    return df[df['distance'] == minim][0]
 
 
 def find_school(logop, psychologist, pedagogue, language, students, school_type):
     data = pd.read_excel(file)
-    df = data.loc[:,['Nazwa', 'Ulica', 'Numer budynku', 'Numer lokalu', 'Kod pocztowy', 'Strona www', data['Publiczność status'].isin(school_type),
-                     data['Kategoria uczniów'].eq(students), data["Języki nauczane"].str.contains(language).any(), data["Czy zatrudnia logopedę"].isin(logop),
-                     data["Czy zatrudnia pedagoga"].isin(pedagogue), data["Czy zatrudnia psychologa"].isin(psychologist)]]
+    df = data[data["Czy zatrudnia psychologa"].isin(psychologist)==True].loc[:,['Nazwa', 'Ulica', 'Numer budynku', 'Numer lokalu', 'Kod pocztowy']]
 
     schools = add_coordinates(df)
 
-    return finder(schools)
+    return schools
--- Nearest_primaryschool_finder/main.py
+++ Nearest_primaryschool_finder/main.py
@@ -1,10 +1,8 @@
 from kivy.app import App
-from kivy.uix.pagelayout import PageLayout
 from kivy.config import Config
+from kivy.uix.pagelayout import PageLayout
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
-from kivy.uix.label import Label
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
+
 import finder
 
 Config.set('graphics', 'resizable', '0')
@@ -13,12 +11,11 @@ Config.set('graphics', 'height', '1500')
 Config.set('graphics', 'fullscreen', '0')
 
 
-
 class Finder(PageLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    information_students_type = ''
+    information_students_type = []
     information_school_type = []
     information_logop_click = []
     information_psychologist_click = []
@@ -26,35 +23,44 @@ class Finder(PageLayout):
     information_language_click = ''
 
     def students_type(self, instance, value, answer):
-        if value == True:
+        if value:
             self.information_students_type = answer
+            return tuple(self.information_students_type)
 
     def school_type(self, instance, value, answer):
-        if value == True:
+        if value:
             self.information_school_type = answer
+            return tuple(self.information_school_type)
 
     def logop_click(self, instance, value, answer):
-        if value == True:
+        if value:
             self.information_logop_click = answer
+            return tuple(self.information_logop_click)
 
     def psychologist_click(self, instance, value, answer):
-        if value == True:
+        if value:
             self.information_psychologist_click = answer
+            return tuple(self.information_psychologist_click)
 
     def pedagogue_click(self, instance, value, answer):
-        if value == True:
+        if value:
             self.information_pedagogue_click = answer
+            return tuple(self.information_pedagogue_click)
 
     def language_click(self, instance, value, answer):
-        if value == True:
+        if value:
             self.information_language_click = answer
-
+            return str(self.information_language_click)
 
     def nearest_school(self):
 
-        df = finder.find_school(self.information_logop_click, self.information_psychologist_click, self.information_pedagogue_click,
-                           self.information_language_click, self.information_students_type, self.information_school_type)
-        self.ids.result.text = finder.finder(df, self.ids.address_city.text, self.ids.address_street.text, self.ids.address_house.text, self.ids.address_code.text)
+        df = finder.find_school(self.information_logop_click, self.information_psychologist_click,
+                                self.information_pedagogue_click,
+                                self.information_language_click, self.information_students_type,
+                                self.information_school_type)
+        self.ids.result.text = finder.finder(df, self.ids.address_city.text, self.ids.address_street.text,
+                                             self.ids.address_house.text, self.ids.address_code.text)
+
 
 class FinderApp(App):
 
